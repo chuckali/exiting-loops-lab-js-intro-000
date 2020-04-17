d595543a4978e05693fe565c77ee46fa9279e718
Charles Cusson <charlescusson@gmail.com>
2020-04-17 02:58:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function breakOut(array, changeValue, stopValue){
     array[i] = changeValue
   }return array
 }
+
+funtion keepGoing(array, changeValue, skipValue){
+  for (let i = 0; i < array.length; i++){
+    if (array[i] === skipValue)
+continue
+array[i] = changeValue
+}return array}
